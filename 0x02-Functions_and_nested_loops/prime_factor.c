b952a4b8f2a6428e872cbcbe0ce558898d66c584
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2023-01-05 20:13:18 +0100

    Fix: fixed a silly bug

---
+++ 0x02-Functions_and_nested_loops/prime_factor.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void prime_number(int n);
+
+int main(void)
+{
+	int num;
+
+	printf("Enter the number to be if it's a prime number:\n");
+	scanf("%d", &num);
+
+	prime_number(num);
+
+	return (0);
+}
+
+void prime_number(int n)
+{
+	int i, check = 0;
+
+	for (i = 2; i <= (n / 2); i++)
+	{
+		if (n % 2 == 0)
+		{
+			check = 1;
+		}
+	}
+
+	if (check == 0)
+	{
+		printf("%d is a prime number.\n", n);
+	}
+	else
+	{
+		printf("%d is not a prime number.\n", n);
+	}
+}
