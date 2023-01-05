dfcba694fd458c224f67fa0e4d156eba5f8d9907
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2023-01-05 19:39:25 +0100

    Added the file

--- 0x02-Functions_and_nested_loops/print_triangle.c
+++ 0x02-Functions_and_nested_loops/print_triangle.c
@@ -7,13 +7,18 @@ int main(void)
 	print_triangle(4);
 }
 
+/**
+ * print_triangle - prints a triangle followed by a new line
+ *
+ *
+ */
 void print_triangle(int n)
 {
 	int i, j, k;
 
 	for (i = 0; i < n; i++)
 	{
-		for (j = n; j > i; j--)
+		for (j = n; j > (i + 1); j--)
 		{
 			putchar(' ');
 		}
