54361192863997e330bcfcda00232e54fdb4b22a
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2023-01-06 19:36:17 +0100

    Update: Added the file

--- 0x02-Functions_and_Loops/prime_factors.c
+++ 0x02-Functions_and_Loops/prime_factors.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 
+void prime_factors(int n);
+
 int main(void)
 {
-	
+	prime_factors(8);
 }
 
 void prime_factors(int n)
 {
+	int i;
 
+	for (i = 2; n > 1; i++)
+	{
+		while (n % i == 0)
+		{
+			printf("%d\n", i);
+			n /= i;
+		}
+	}
 }
