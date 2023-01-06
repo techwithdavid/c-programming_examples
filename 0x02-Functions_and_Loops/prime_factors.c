89c05bb626677ca555345345c4bc02db9ea7ff4e
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2023-01-06 18:00:56 +0100

    Update: Added the file

---
+++ 0x02-Functions_and_Loops/prime_factors.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	
+}
+
+void prime_factors(int n)
+{
+
+}
---
+++ 0x02-Functions_and_Loops/print_triangle.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void print_triangle(int n);
+
+int main(void)
+{
+	print_triangle(4);
+}
+
+/**
+ * print_triangle - prints a triangle followed by a new line
+ * @n: argument to the function
+ * Return: void
+ */
+void print_triangle(int n)
+{
+	int i, j, k;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = n; j > (i + 1); j--)
+		{
+			putchar(' ');
+		}
+
+		for (k = 0; k <= i; k++)
+		{
+			putchar('#');
+		}
+		putchar(10);
+	}
+}
