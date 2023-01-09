32ec373ca246f540484421897a24957a57a2b620
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2023-01-09 22:37:57 +0100

    Added the file

---
+++ 0x04-Pointers/puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void _puts(char *str);
+
+int main(void)
+{
+	char *str;
+
+	str = "C is fun";
+	_puts(str);
+
+	return (0);
+}
+
+void _puts(char *str)
+{
+	int i;
+	char res[30];
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		res[i] = str[i];
+	}
+	printf("%s\n", res);
+}
