88fb5ac68b7b91390f1bd229637de83daea37cc0
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2023-01-10 11:42:07 +0100

    Update: Added the code's logic

--- 0x04-Pointers/print_rev.c
+++ 0x04-Pointers/print_rev.c
@@ -4,10 +4,30 @@ void print_rev(char *s);
 
 int main(void)
 {
+	char *str, res;
+
+	str = "This will be printed backward";
+	print_rev(str);
 
 }
 
 void print_rev(char *s)
 {
-	
+	int i, j, max;
+	char rev[40];
+
+	max = 0;
+	while (s[max] != '\0')
+	{
+		max++;
+	}
+	i = max - 1;
+
+	for (j = 0; j < max; j++)
+	{
+		rev[j] = s[i];
+		i--;
+	}
+
+	printf("%s\n", rev);
 }
