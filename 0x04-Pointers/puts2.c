cf4c7dec9c98322c2b8ef3f6c42d98d64e81dbfb
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2023-01-10 13:49:25 +0100

    Update: Added the code's logic

--- 0x04-Pointers/puts2.c
+++ 0x04-Pointers/puts2.c
@@ -4,10 +4,22 @@ void puts2(char *str);
 
 int main(void)
 {
-	puts();
+	char *str;
+
+	str = "0123456789";
+	puts2(str);
 }
 
 void puts2(char *str)
 {
-	
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			putchar(str[i]);
+		}
+	}
+	putchar(10);
 }
